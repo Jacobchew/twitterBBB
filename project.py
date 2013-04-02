e18df36fde6e417976b364ca70c7158d61a2fe7a
Afeez Aziz <afeezaziz@gmail.com>
2013-04-02 18:41:03 +0100

    Edited port second timr

--- project.py
+++ project.py
@@ -19,7 +19,7 @@ def things():
 
 if __name__ == '__main__':
 	app.debug = True
-	port = int(os.environ.get('PORT', 2002))
-	app.run(host = '127.0.0.1', port = port)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host = '0.0.0.0', port = port)
     
 
