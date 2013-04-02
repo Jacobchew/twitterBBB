a3bd9ed978b87c5730cf7bea9fae0c63a1ae7f47
unknown <leon_yuenjin@hotmail.com>
2013-04-02 15:22:53 +0100

    For attention afeez

--- project1.py
+++ project1.py
@@ -1,18 +1,12 @@
 import os,re, math
 
 from flask import Flask, render_template, request ,url_for
-import jacobfunc
 
 app = Flask(__name__)
 
 @app.route('/sexyitems')
 def things():
 	return render_template('things.html')
-@app.route('/')
-def index():
-	listoftweets = jacobfunc.jinbanyakmain()
-	tweet = listoftweets['statuses']
-	return render_template('index.html', data=tweet)	
 	
 if __name__ == '__main__':
 	app.debug = True
