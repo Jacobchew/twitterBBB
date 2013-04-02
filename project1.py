ec3aea6b5bd2059f7927882edbaf531cc1ff4cfe
unknown <leon_yuenjin@hotmail.com>
2013-04-02 14:16:49 +0100

    saya main banyak

--- project1.py
+++ project1.py
@@ -1,14 +1,14 @@
 import os,re, math
 
-from flask import Flask, render_template, request 
+from flask import Flask, render_template, request ,url_for
 
 app = Flask(__name__)
 
-@app.route('/sexyiteams')
+@app.route('/sexyitems')
 def things():
 	return render_template('things.html')
 	
-if __name__=__main__:
-	app.debug=True
+if __name__ == '__main__':
+	app.debug = True
 	port = int(os.environ.get('PORT', 2002))
-	app.run(host='126.0.0.1', port=port) 
+	app.run(host='127.0.0.1', port=port) 
